23310d3269099047e197282395c93c1ccee98c64
Ozan Akyildiz <oakyildiz@wpi.edu>
2018-01-31 04:29:11 -0500

    new flies for the outlined packages

---
+++ jetson_ws/src/beat_player/scripts/beat_player.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import pygame.midi as midi
+import time
+from playsound import playsound
+import rospy
+from beat_msgs.msg import Beat
+import csv
+
+
+class Player(object):
+	note='../res/kick.mp3'
+
+	rate=10
+	def __init__(self):
+		
+		rospy.init_node('beat_player_node', anonymous=True)
+		self.mode = rospy.get_param('~player_mode', 'playback')
+		
+		self.r = rospy.Rate(self.rate) # 10 Hz
+		
+		if self.mode is not 'playback':  
+			self.beat_sub = rospy.Subscriber('beats', Beat, self.play)
+			self.run=self.live
+		else:
+			self.run=self.playback
+			
+			
+	def play(self,data):
+		#playsound(self.note) # this has dep issues
+		print('beep') #for now, let's find a sound module. 
+	
+	def loadfile(self):
+			raise NotImplemented #convert read string to floats
+		try:
+			path=input('Beat file:')
+			
+		except SyntaxError:
+			path = 'txt/test.txt'
+		if not path:
+			path='txt/test.txt'
+		return open(path, 'rU')
+		
+	def playback(self):
+		print("Playing form file")
+	
+		T=1/self.rate #1/f, Period
+		beatfile=self.loadfile()
+		beats=beatfile.readlines()
+		# for multicloumn
+		# csv.reader(beats)
+		i=0
+		mark = rospy.Time.now()			
+		while i < len(beats):
+			elapsed=rospy.Time.now() - mark
+			if abs(elapsed-beats[i]) < T: #if we are in the correct t with a resolution of 1/rate	
+				self.play()
+				i+=1
+							
+	def live(self):
+		print("Player is live")
+		while not rospy.is_shutdown():
+	 		
+			self.r.sleep()	
+
+if __name__ == '__main__':
+    try:
+        player = Player()
+        player.run()
+    except rospy.ROSInterruptException: pass
+
+
+
