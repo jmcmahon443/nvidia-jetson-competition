78edb7c3b4cb4ce6fa84a544ae36ece84941a73f
Ozan Akyildiz <oakyildiz@wpi.edu>
2018-02-12 19:16:10 -0500

    name change, receiving beats, almost ready to make basic predicitons then to branch it out for multifit

--- jetson_ws/src/master_of_rhythm/scripts/beat_player.py
+++ jetson_ws/src/master_of_rhythm/scripts/beat_player.py
@@ -4,7 +4,7 @@ from __future__ import division
 import pygame.midi as midi
 import pygame
 import time
-from playsound import playsound
+#from playsound import playsound
 import rospy
 from beat_msgs.msg import Beat
 import csv
@@ -12,67 +12,65 @@ import csv
 
 class Player(object):
 	kick='res/kick.mp3'
- 
-    
+
+
 
 	rate=10
 	def __init__(self):
 		#pygame.init()
 		#self.s = pygame.mixer.Sound(Player.kick)
-    	
+		self.beats=[]
 		rospy.init_node('beat_player_node', anonymous=True)
-		self.mode = rospy.get_param('~player_mode', 'playback')
-		
+		self.mode = rospy.get_param('~player_mode', 'live')
+
 		self.r = rospy.Rate(self.rate) # 10 Hz
-		
-		if self.mode is not 'playback':  
+
+		if self.mode is not 'playback':
 			self.beat_sub = rospy.Subscriber('beats', Beat, self.play)
 			self.run=self.live
 		else:
 			self.run=self.playback
-			
-			
-	def play(self):
+
+
+	def play(self,data):
 		#playsound(self.note) # this has dep issues
-		print('beep') #for now, let's find a sound module. 
+		print('beep') #for now, let's find a sound module.
+		self.beats.append(data.header.stamp)
 		#self.s.play()
 	def loadfile(self):
 		try:
 			path=input('Beat file:')
-			
+
 		except SyntaxError:
 			path = 'txt/test.txt'
 		if not path:
 			path='txt/test.txt'
 		return open(path, 'rU')
-		
+
 	def playback(self):
 		print("Playing form file")
-	
+
 		T=1/self.rate #1/f, Period
 		beatfile=self.loadfile()
 		beats=beatfile.readlines()
 		# for multicloumn
 		# csv.reader(beats)
 		i=0
-		mark = rospy.Time.now()			
+		mark = rospy.Time.now()
 		while i < len(beats):
 			elapsed=rospy.Time.now() - mark
-			if abs(elapsed.to_sec()-float(beats[i])) < T: #if we are in the correct t with a resolution of 1/rate	
+			if abs(elapsed.to_sec()-float(beats[i])) < T: #if we are in the correct t with a resolution of 1/rate
 				self.play()
 				i+=1
-							
+
 	def live(self):
 		print("Player is live")
 		while not rospy.is_shutdown():
-	 		
-			self.r.sleep()	
+
+			self.r.sleep()
 
 if __name__ == '__main__':
     try:
         player = Player()
         player.run()
     except rospy.ROSInterruptException: pass
-
-
-
